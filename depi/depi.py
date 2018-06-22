e4b59210141621384266b99686ad27e27467180f
Antonino Ingargiola <tritemio@gmail.com>
2018-06-22 00:03:36 -0700

    Add fit_name to save/load_params()

--- depi/depi.py
+++ depi/depi.py
@@ -22,7 +22,7 @@ def recolor_burstsph_cache(timestamp, seed=1, **params):
     return burstsph
 
 
-def save_params(fname, params, bounds=None):
+def save_params(fname, params, bounds=None, fit_name=None):
     """Save the simulation parameters `params` to disk."""
     if not fname.lower().endswith('.json'):
         fname = fname + '.json'
@@ -30,7 +30,9 @@ def save_params(fname, params, bounds=None):
         raise IOError(f'File {fname} aready exists. '
                       f'Please move it before saving with the same name.')
     if bounds is not None:
-        params = {**params, bounds: bounds}  # doesn't modify the original params
+        params = {**params, 'bounds': bounds}
+    if fit_name is not None:
+        params = {**params, 'fit_name': fit_name}
     with open(fname, 'wt') as f:
         json.dump(params, f, ensure_ascii=False, indent=4)
 
@@ -42,7 +44,8 @@ def load_params(fname):
     with open(fname) as f:
         params = json.load(f)
     bounds = params.pop('bounds', None)
-    return params, bounds
+    fit_name = params.pop('fit_name', None)
+    return params, bounds, fit_name
 
 
 def validate_params(params):
